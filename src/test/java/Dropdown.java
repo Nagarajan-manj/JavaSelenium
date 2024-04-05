6e5900c256422368cade46b2a891e9076f11382e
Nagaraj <bnagarajan35@gmail.com>
2024-04-05 13:32:08 +0530

    third commit

--- src/test/java/Dropdown.java
+++ src/test/java/Dropdown.java
@@ -1,7 +1,7 @@
 class Dropdown
   {
-    public static void main(STring args[])
+    public static void main(String args[])
     {
-      System.out.println(Welcome to dropdown check");
+      System.out.println("Welcome to dropdown check");
   }
   }
--- src/test/java/Logintestcase/Demo.java
+++ src/test/java/Logintestcase/Demo.java
@@ -1,66 +1,20 @@
 package Logintestcase;
 
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 
 public class Demo {
-//    @Test
-//    public void testCase1() {
-//        System.out.println("This is the A Normal Test Case1");
-//    }
-//
-//
-//    @BeforeMethod
-//    public void beforeMethod() {
-//        System.out.println("This will execute before every Method");
-//    }
-
-    @Test(invocationCount = 3)
-    public void testCase2() {
-        System.out.println("This is the A Normal Test Case2");
+    @DataProvider(name = "data-provider")
+    public Object[][] dpMethod(){
+        return new Object[][] {{2, 3 , 5},{1,1,3}};
     }
 
-//
-////    @AfterMethod
-//    public void afterMethod() {
-//        System.out.println("This will execute after every Method");
-//    }
-//
-//    @BeforeClass
-//    public void beforeClass() {
-//        System.out.println("This will execute before the Class");
-//    }
-//
-//    @AfterClass
-//    public void afterClass() {
-//        System.out.println("This will execute after the Class");
-//    }
-//
-//    @BeforeTest
-//    public void beforeTest() {
-//        System.out.println("This will execute before the Test");
-//    }
-//
-//    @AfterTest
-//    public void afterTest() {
-//        System.out.println("This will execute after the Test");
-//    }
-//
-//    @BeforeSuite
-//    public void beforeSuite() {
-//        System.out.println("This will execute before the Test Suite");
-//    }
-//
-//    @AfterSuite
-//    public void afterSuite() {
-//        System.out.println("This will execute after the Test Suite");
-//    }
+    @Test (dataProvider = "data-provider")
+    public void myTest (int a, int b, int result) {
+        int sum = a + b;
+        System.out.println("The result is:==," +result);
+        System.out.println("The sum is:==,"+sum);
+        Assert.assertEquals(result, sum);
+    }
 }
